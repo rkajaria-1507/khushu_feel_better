9597ac069d9e340f2de486048b0325367e6e1e1f
rkajaria-1507 <69714674+rkajaria-1507@users.noreply.github.com>
2025-09-13 15:49:54 +0530

    Track media files with LFS

--- virtual-hug/public/js/bouquet.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const bodyEl = document.body;
-  if (bodyEl && bodyEl.classList.contains('container')) {
-    bodyEl.classList.remove('container');
-  }
-});
